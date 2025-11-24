713af635ee2c2a6eb03b359a1c9f9ca8a2ae7987
Zhongyuwang <zhongyuw@seas.upenn.edu>
2025-11-23 22:07:51 -0500

    1

--- sensor_shell/twister-out/qemu_cortex_m3_ti_lm3s6965/zephyr/samples/sensor/sensor_shell/tests/balance/horse.balance.unit/zephyr/zephyr.dts.d
+++
@@ -1,5 +0,0 @@
-empty_file.o: C:/ncs/v3.0.2/zephyr/misc/empty_file.c \
- C:/ncs/v3.0.2/zephyr/boards/qemu/cortex_m3/qemu_cortex_m3.dts \
- C:/ncs/v3.0.2/zephyr/dts/arm/ti/lm3s6965.dtsi \
- C:/ncs/v3.0.2/zephyr/dts/arm/armv7-m.dtsi \
- C:/ncs/v3.0.2/zephyr/dts/common/skeleton.dtsi
